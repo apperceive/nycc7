2af77413f11d969e805b3988bc8adc45ceeb0217
Markus Sandy <markus.sandy@gmail.com>
2015-09-27 23:20:04 -0400

    update field_group module,hide obsolete and settings in ride ad/edit

--- sites/all/themes/nycc7/templates/node--rides_edit.tpl.php
+++ sites/all/themes/nycc7/templates/node--rides_edit.tpl.php
@@ -31,62 +31,48 @@
   // move to nycc_rides_processor_node_ride_edit()
   $op = (arg(1) == 'add') ? 'add' : ((arg(2) == 'edit') ? 'edit' : 'noop');
   
-  $can_approve = function_exists('nycc_rides_can_approve') ? nycc_rides_can_approve() : true;
+  $can_approve = function_exists('nycc_rides_can_approve') ? nycc_rides_can_approve() : false;
   
   if (!$can_approve) {
     hide($form['group_rides_htabs']['group_ride_rc_info']);
     hide($form['group_rides_htabs']['group_ride_participants']['group_rides_participants']['group_rides_riders']);
     hide($form['group_rides_htabs']['group_ride_participants']['group_rides_participants']['group_rides_waitlist']);
       
-    // hide($form['group_rides_htabs']['group_ride_participants']['group_rides_participants']['group_rides_attendees']);
-  } // can_approve
-
+    //hide($form['group_rides_htabs']['group_ride_participants']['group_rides_participants']['group_rides_attendees']);
+  } 
   else {
+    /*
     $form['group_rides_htabs']['group_ride_rc_info']['revs'] = array(
       '#markup' => drupal_render($form['revision_information']),
     );    
+    */
   }
+  // hide extra fields from all unless '/admin' is appended to url
+  if (arg(3) != 'admin') {
+    hide($form['group_rides_htabs']['group_rides_advanced']);
+    hide($form['additional_settings']);
+  }
+  
   
   // where is best place to do this? does not work in hook_form_alter as 'group_rides_htabs' key does not yet exist yet
       
   //dpm($form['group_rides_htabs']['group_ride_participants']['group_rides_participants']);
   
-  $form['group_rides_htabs']['group_ride_participants']['group_rides_participants']['group_rides_leaders']['field_ride_current_leaders']['und']['add_more']['#value'] = t("Add another ride leader");
+  $form['group_rides_htabs']['group_ride_participants']['group_rides_participants']['group_rides_leaders']['field_ride_current_leaders']['und']['add_more']['#value'] = t("Add ride leader");
   
-  $form['group_rides_htabs']['group_ride_participants']['group_rides_participants']['group_rides_riders']['field_ride_current_riders']['und']['add_more']['#value'] = t("Add another rider");
+  $form['group_rides_htabs']['group_ride_participants']['group_rides_participants']['group_rides_riders']['field_ride_current_riders']['und']['add_more']['#value'] = t("Add rider");
   
   $form['group_rides_htabs']['group_ride_participants']['group_rides_participants']['group_rides_waitlist']['field_ride_waitlist']['und']['add_more']['#value'] = t("Add to waitlist");
   
-  /*
-  $form['group_rides_htabs']['group_ride_participants']['group_rides_participants']['group_rides_attendees']['field_ride_attendees']['und']['add_more']['#value'] = t("Add another attendee");
-  */
+  $form['group_rides_htabs']['group_ride_participants']['group_rides_participants']['group_rides_attendees']['field_ride_attendees']['und']['add_more']['#value'] = t("Add attendee");
   
+  
+  // note various spellings of attachments/attachements. ugh!
   //dpm($form['group_rides_htabs']['group_ride_attachements']);
   
   $form['group_rides_htabs']['group_ride_attachements']['field_ride_image']['und']['#file_upload_title'] = t("Add another image");
   
-  // note various spellings of attachments/attachements. ugh!
   $form['group_rides_htabs']['group_ride_attachements']['field_ride_attachments']['und']['#file_upload_title'] = t("Add another attachment");
-
-  /*
-  $form['group_rides_htabs']['group_rides_info']['field_ride_timestamp']['und'][0]['value']['additional_dates'] = array(
-    '#type' => 'textfield',
-    '#title' => 'Additional dates',
-    '#description' => 'Optional. Create additional submissions based on this one, but for the list of dates entered here. Separate multiple dates by commas.',
-    '#weight' => 10,
-    '#value' => '',           // why is this is required for node form but not for general form!!!
-    '#prefix' => '',
-    '#suffix' => '',
-    '#id' => 'edit-nycc-additional-dates',
-    '#input' => true,                            // why needed?
-    '#name' => 'additional_dates',
-    '#attributes' => array('class' => array('datepicker')),
-    //'#cols' => 60,
-    //'#rows' => 1,
-    //'#default_value' => 'testing 1 2 3',  // not used? again, diff from general form
-    //'#resizable' => false,  
-  ); 
-  */
   
   if ($op == 'add') {
   }
@@ -102,8 +88,7 @@
   $output .= drupal_render_children($form);
   
   $output .= drupal_render($form['actions']['submit']);
-  //$output .= drupal_render($form['actions']['submit_ride']);
-  
+
 ?>
 
 <?php print $output;  ?>
