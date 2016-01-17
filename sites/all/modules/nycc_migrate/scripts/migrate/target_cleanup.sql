8383e67928566b800ac78236cc746b5d967d589d
Markus Sandy <markus.sandy@gmail.com>
2016-01-17 11:45:12 -0500

    DRUPAL UPDATE: link, moneysuite

--- sites/all/modules/nycc_migrate/scripts/migrate/target_cleanup.sql
+++ sites/all/modules/nycc_migrate/scripts/migrate/target_cleanup.sql
@@ -9,3 +9,6 @@ INSERT INTO users_roles (uid, rid) VALUES (3692, 6);
 UPDATE `field_data_field_ride_current_leaders` l INNER JOIN node n ON n.nid=l.field_ride_current_leaders_uid SET field_ride_current_leaders_uid =n.uid AND n.type= 'profile';
 UPDATE `field_revision_field_ride_current_leaders` l INNER JOIN node n ON n.nid=l.field_ride_current_leaders_uid SET field_ride_current_leaders_uid =n.uid AND n.type= 'profile';
 
+
+DELETE FROM node WHERE type IN ('gpcaptspost', 'test_users', 'block_page', 'poll2', 'webforms')
+/* TODO: clear out all field data and rev tables that have invalid entity_id's */
